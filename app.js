b66e3ab64fe525a27ba68c4fbabb17c3117aed87
lamlance <lamhoangdien113@gmail.com>
2022-11-26 14:34:01 +0700

    init

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const expressHandlebars = require("express-handlebars");
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+app.set("port",port);
+app.set('views', __dirname + '/views');
+app.use(express.static(`${__dirname}/public`));
+
+
+app.engine("hbs",expressHandlebars.engine({
+  layoutsDir:`${__dirname}/views/layouts`,
+  partialsDir:`${__dirname}/views/partials`,
+  defaultLayout:"layout",
+  extname:"hbs"
+}));
+app.set("view engine","hbs");
+
+app.get("/",(req,res)=>{
+  res.render("index");
+});
+
+app.listen(app.get("port"),()=>{
+  console.log(`App listening on http://localhost:${port}`);
+});
